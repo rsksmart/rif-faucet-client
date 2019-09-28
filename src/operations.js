dde6fdc5e1fd2a9aa38e6249b6b4d2dd73dc9523
jonathansmirnoff <jonathan@rsk.co>
2019-09-28 13:59:32 -0300

    Implemented logic to grab the minimum gas price

--- src/operations.js
+++ src/operations.js
@@ -39,30 +39,32 @@ export const dispense = () => dispatch => {
   if (!window.ethereum || (window.ethereum.networkVersion !== undefined && window.ethereum.networkVersion !== config.networkId))
       return dispatch(errorDispense('Connect to RSK Testnet'));
 
-  window.ethereum.enable()
-  .then(accounts => {    
-    const web3 = new Web3(window.ethereum, null, { defaultAccount: accounts[0], defaultGasPrice: 59240000 });
-    const faucet = new web3.eth.Contract([
-      {
-        'constant': false,
-        'inputs': [
-          {
-            'name': 'to',
-            'type': 'address'
-          }
-        ],
-        'name': 'dispense',
-        'outputs': [],
-        'payable': false,
-        'stateMutability': 'nonpayable',
-        'type': 'function',
-        'signature': '0x5f746233'
-      }
-    ], config.faucet);
+  getMinimunGasPrice().then( gmp => {
+    window.ethereum.enable()
+    .then(accounts => {      
+      const web3 = new Web3(window.ethereum, null, { defaultAccount: accounts[0], defaultGasPrice: gmp });
+      const faucet = new web3.eth.Contract([
+        {
+          'constant': false,
+          'inputs': [
+            {
+              'name': 'to',
+              'type': 'address'
+            }
+          ],
+          'name': 'dispense',
+          'outputs': [],
+          'payable': false,
+          'stateMutability': 'nonpayable',
+          'type': 'function',
+          'signature': '0x5f746233'
+        }
+      ], config.faucet);
 
-    faucet.methods.dispense(accounts[0]).send()
-    .on('transactionHash', hash => dispatch(receiveDispense(hash)))
-    .catch(error => dispatch(errorDispense(error.message)));
+      faucet.methods.dispense(accounts[0]).send()
+      .on('transactionHash', hash => dispatch(receiveDispense(hash)))
+      .catch(error => dispatch(errorDispense(error.message)));
+    })
   })
   .catch(e => dispatch(errorDispense(e.message)));
 }
@@ -81,3 +83,13 @@ export const getNetwork = () => dispatch => {
     dispatch(receiveNetwork(res));
   });
 };
+
+async function getMinimunGasPrice(){
+  const web3 = new Web3(config.publicNode);
+  var block = await web3.eth.getBlock("latest");  
+  if (block.minimumGasPrice <= 1) {
+    return 1;
+  } else {
+    return block.minimumGasPrice * 1.01;
+  }
+}
